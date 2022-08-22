c9ae7d9fd3ce6cb6d84935261f0725073802ea1d
EstexNT <estex64@gmail.com>
2022-08-23 01:44:42 +0300

    split up to RevoSDK/os/OSFont

---
+++ asm/RevoSDK/os/OSArena.s
@@ -0,0 +1,134 @@
+.include "macros.inc"
+
+.section .text, "ax"  # 0x800D8FF0 - 0x801D3284
+
+.global func_800DB0C0
+func_800DB0C0:
+/* 800DB0C0 000D69E0  80 6D 97 90 */	lwz r3, lbl_80320650-_SDA_BASE_(r13)
+/* 800DB0C4 000D69E4  4E 80 00 20 */	blr 
+/* 800DB0C8 000D69E8  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB0CC 000D69EC  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB0D0
+func_800DB0D0:
+/* 800DB0D0 000D69F0  80 6D 97 94 */	lwz r3, lbl_80320654-_SDA_BASE_(r13)
+/* 800DB0D4 000D69F4  4E 80 00 20 */	blr 
+/* 800DB0D8 000D69F8  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB0DC 000D69FC  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB0E0
+func_800DB0E0:
+/* 800DB0E0 000D6A00  80 6D 97 90 */	lwz r3, lbl_80320650-_SDA_BASE_(r13)
+/* 800DB0E4 000D6A04  4E 80 00 20 */	blr 
+/* 800DB0E8 000D6A08  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB0EC 000D6A0C  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB0F0
+func_800DB0F0:
+/* 800DB0F0 000D6A10  80 6D 8B E0 */	lwz r3, lbl_8031FAA0-_SDA_BASE_(r13)
+/* 800DB0F4 000D6A14  4E 80 00 20 */	blr 
+/* 800DB0F8 000D6A18  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB0FC 000D6A1C  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB100
+func_800DB100:
+/* 800DB100 000D6A20  80 6D 8B E4 */	lwz r3, lbl_8031FAA4-_SDA_BASE_(r13)
+/* 800DB104 000D6A24  4E 80 00 20 */	blr 
+/* 800DB108 000D6A28  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB10C 000D6A2C  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB110
+func_800DB110:
+/* 800DB110 000D6A30  80 6D 8B E0 */	lwz r3, lbl_8031FAA0-_SDA_BASE_(r13)
+/* 800DB114 000D6A34  4E 80 00 20 */	blr 
+/* 800DB118 000D6A38  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB11C 000D6A3C  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB120
+func_800DB120:
+/* 800DB120 000D6A40  90 6D 97 90 */	stw r3, lbl_80320650-_SDA_BASE_(r13)
+/* 800DB124 000D6A44  4E 80 00 20 */	blr 
+/* 800DB128 000D6A48  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB12C 000D6A4C  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB130
+func_800DB130:
+/* 800DB130 000D6A50  90 6D 97 94 */	stw r3, lbl_80320654-_SDA_BASE_(r13)
+/* 800DB134 000D6A54  4E 80 00 20 */	blr 
+/* 800DB138 000D6A58  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB13C 000D6A5C  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB140
+func_800DB140:
+/* 800DB140 000D6A60  90 6D 97 90 */	stw r3, lbl_80320650-_SDA_BASE_(r13)
+/* 800DB144 000D6A64  4E 80 00 20 */	blr 
+/* 800DB148 000D6A68  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB14C 000D6A6C  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB150
+func_800DB150:
+/* 800DB150 000D6A70  90 6D 8B E0 */	stw r3, lbl_8031FAA0-_SDA_BASE_(r13)
+/* 800DB154 000D6A74  4E 80 00 20 */	blr 
+/* 800DB158 000D6A78  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB15C 000D6A7C  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB160
+func_800DB160:
+/* 800DB160 000D6A80  90 6D 8B E4 */	stw r3, lbl_8031FAA4-_SDA_BASE_(r13)
+/* 800DB164 000D6A84  4E 80 00 20 */	blr 
+/* 800DB168 000D6A88  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB16C 000D6A8C  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB170
+func_800DB170:
+/* 800DB170 000D6A90  90 6D 8B E0 */	stw r3, lbl_8031FAA0-_SDA_BASE_(r13)
+/* 800DB174 000D6A94  4E 80 00 20 */	blr 
+/* 800DB178 000D6A98  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB17C 000D6A9C  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+.global func_800DB180
+func_800DB180:
+/* 800DB180 000D6AA0  80 0D 8B E0 */	lwz r0, lbl_8031FAA0-_SDA_BASE_(r13)
+/* 800DB184 000D6AA4  38 A4 FF FF */	addi r5, r4, -1
+/* 800DB188 000D6AA8  7C A6 28 F8 */	nor r6, r5, r5
+/* 800DB18C 000D6AAC  7C A4 02 14 */	add r5, r4, r0
+/* 800DB190 000D6AB0  38 05 FF FF */	addi r0, r5, -1
+/* 800DB194 000D6AB4  7C C0 00 38 */	and r0, r6, r0
+/* 800DB198 000D6AB8  7C 60 1A 14 */	add r3, r0, r3
+/* 800DB19C 000D6ABC  7C 83 22 14 */	add r4, r3, r4
+/* 800DB1A0 000D6AC0  7C 03 03 78 */	mr r3, r0
+/* 800DB1A4 000D6AC4  38 04 FF FF */	addi r0, r4, -1
+/* 800DB1A8 000D6AC8  7C C0 00 38 */	and r0, r6, r0
+/* 800DB1AC 000D6ACC  90 0D 8B E0 */	stw r0, lbl_8031FAA0-_SDA_BASE_(r13)
+/* 800DB1B0 000D6AD0  4E 80 00 20 */	blr 
+/* 800DB1B4 000D6AD4  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB1B8 000D6AD8  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+/* 800DB1BC 000D6ADC  00 00 00 00 */	.4byte 0x00000000  /* unknown instruction */
+
+
+.section .sdata, "wa"  # 0x8031FA98 - 0x8031FFD8
+
+.global lbl_8031FAA0
+
+lbl_8031FAA0:
+
+	# ROM: 0x31BBA0
+	.4byte 0xFFFFFFFF
+
+
+.global lbl_8031FAA4
+
+lbl_8031FAA4:
+
+	# ROM: 0x31BBA4
+	.4byte 0xFFFFFFFF
+
+
+.section .sbss, "wa"  # 0x803205F0 - 0x80320F80
+
+.global lbl_80320650
+lbl_80320650:
+	.skip 0x4
+.global lbl_80320654
+lbl_80320654:
+	.skip 0x4
--- obj_files_rvlsdk.mk
+++ obj_files_rvlsdk.mk
@@ -2,4 +2,35 @@ RVL_SDK_BASE_O_FILES :=\
 	$(BUILD_DIR)/asm/RevoSDK/base/PPCArch.o
 
 RVL_SDK_OS_O_FILES :=\
-	todo
+	$(BUILD_DIR)/asm/RevoSDK/os/OS.o \
+	$(BUILD_DIR)/asm/RevoSDK/os/OSAlarm.o \
+	$(BUILD_DIR)/asm/RevoSDK/os/OSAlloc.o \
+	$(BUILD_DIR)/asm/RevoSDK/os/OSArena.o \
+	$(BUILD_DIR)/asm/RevoSDK/os/OSAudioSystem.o \
+	$(BUILD_DIR)/asm/RevoSDK/os/OSCache.o \
+	$(BUILD_DIR)/asm/RevoSDK/os/OSContext.o \
+	$(BUILD_DIR)/asm/RevoSDK/os/OSError.o \
+	$(BUILD_DIR)/asm/RevoSDK/os/OSExec.o \
+	$(BUILD_DIR)/asm/RevoSDK/os/OSFatal.o \
+	$(BUILD_DIR)/asm/RevoSDK/os/OSFont.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSInterrupt.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSLink.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSMessage.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSMemory.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSMutex.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSReboot.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSReset.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSRtc.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSSync.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSThread.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSTime.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSUtf.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSIpc.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSStateTM.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSPlayRecord.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSStateFlags.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSNet.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSNandbootInfo.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSPlayTime.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/OSLaunch.o 
+#	$(BUILD_DIR)/asm/RevoSDK/os/__ppc_eabi_init.o 
