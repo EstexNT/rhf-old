0d12d212d7415b2b5116a803daf92caa22db0f9f
EstexNT <estex64@gmail.com>
2022-09-04 20:35:53 +0300

    add a general RVL_SDK variable

--- obj_files.mk
+++ obj_files.mk
@@ -1,5 +1,18 @@
 include obj_files_rvlsdk.mk
 
+RVL_SDK_O_FILES :=\
+	$(RVL_SDK_BASE_O_FILES) \
+	$(RVL_SDK_OS_O_FILES) \
+	$(RVL_SDK_MTX_O_FILES) \
+	$(RVL_SDK_DVD_O_FILES) \
+	$(RVL_SDK_VI_O_FILES) \
+	$(RVL_SDK_PAD_O_FILES) \
+	$(RVL_SDK_AI_O_FILES) \
+	$(RVL_SDK_AX_O_FILES) \
+	$(RVL_SDK_AXFX_O_FILES) \
+	$(RVL_SDK_DSP_O_FILES) \
+	$(RVL_SDK_GX_O_FILES)
+
 INIT_O_FILES :=\
 	$(BUILD_DIR)/asm/init.o
 
